3f5929d2e7fe3cd850b34a843f02fb099937a400
igorpfarias <igorfarias219@gmail.com>
2026-03-18 20:14:53 -0300

    Exercicios

---
+++ EntradaDeDadosSoma.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n1, n2, soma;
+
+    cout << "Digite N1: ";
+    cin >> n1;
+
+    cout << "Digite N2: ";
+    cin >> n2;
+
+    soma = n1 + n2;
+
+    cout << "Soma de N1 e N2 igual: " << soma;
+
+    return 0;
+
+}
---
+++ Soma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+
+    int a,b,soma;
+
+    a = 5;
+    b = 7;
+    
+    soma = a + b;
+
+    cout << "Soma: " << soma;
+
+    return 0;
+
+}
+
